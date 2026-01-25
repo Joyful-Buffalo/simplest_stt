61a9cc7b788840ec3d50701afdbc766676f072ed
Joyful Buffalo <3072806964@qq.com>
2026-01-25 19:22:11 +0800

    fix none process

--- utlis/CharTokenizer.py
+++ utlis/CharTokenizer.py
@@ -3,16 +3,16 @@ from typing import List
 
 
 class CharTokenizer:
-    def __init__(self, chars: str=None, jsonls:List[str]=None):
-        self.jsonls = jsonls
-        self.chars = chars
+    def __init__(self, chars: str=None, jsonl_paths:List[str]=None):
+        self.jsonl_paths = jsonl_paths if jsonl_paths is not None else []
+        self.chars = chars if chars is not None else ''
         self.chars = self.build_chars()
-        self.char2idx = {char: idx for idx, char in enumerate(chars)}
-        self.idx2char = {idx: char for idx, char in enumerate(chars)}
+        self.char2idx = {char: idx for idx, char in enumerate(self.chars)}
+        self.idx2char = {idx: char for idx, char in enumerate(self.chars)}
 
     def build_chars(self):
         char_set = set()
-        for jsonl in self.jsonls:
+        for jsonl in self.jsonl_paths:
             with open(jsonl, 'r', encoding='utf-8') as f:
                 for line in f:
                     data = json.loads(line.strip())
