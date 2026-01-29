3fded52fabda5af22b3078c423cba7d288acb0e8
Joyful Buffalo <3072806964@qq.com>
2026-01-29 20:51:57 +0800

    add config

--- utils/JsonlASRDataset.py
+++ utils/JsonlASRDataset.py
@@ -3,14 +3,18 @@ import torch
 from torch.utils.data import Dataset
 import torchaudio
 
-from CharTokenizer import CharTokenizer
+from utils.CharTokenizer import CharTokenizer
 
 class JsonlASRDataset(Dataset):
-    def __init__(self, jsonl_file_path:str, tokenizer:CharTokenizer=None):
+    def __init__(self, jsonl_file_path:str, config, tokenizer:CharTokenizer=None):
         super().__init__()
-        self.jsonl_file_path = jsonl_file_path
-        self.json_list = self.build_list()
+        self.fbank_config = config['fbank_config']
+        self.frame_length_ms = self.fbank_config['frame_length_ms']
+        self.frame_shift_ms = self.fbank_config['frame_shift_ms']
         self.tokenizer = tokenizer
+        self.jsonl_file_path = jsonl_file_path
+        self.num_frames = []
+        self.json_list = self.build_list()
 
     def build_list(self):
         json_list = []
@@ -18,6 +22,9 @@ class JsonlASRDataset(Dataset):
             for line in f:
                 data = json.loads(line.strip())
                 json_list.append(data)
+                duration = data['duration']
+                num_frame = int((duration*1000 - self.frame_length_ms) / self.frame_shift_ms) + 1
+                self.num_frames.append(num_frame)
         return json_list
     
     def __len__(self):
@@ -27,8 +34,19 @@ class JsonlASRDataset(Dataset):
         item = self.json_list[idx]
         path = item['path']
         txt = item['txt']
-        wav, *_ = torchaudio.load(path)
-        fbank  = torchaudio.compliance.kaldi.fbank(wav)
+        wav, sample_rate = torchaudio.load(path)
+        expected_rate = self.fbank_config['sample_rate']
+        if sample_rate != expected_rate:
+            wav = torchaudio.functional.resample(wav, sample_rate, expected_rate)
+            sample_rate = expected_rate
+        fbank = torchaudio.compliance.kaldi.fbank(
+            wav,
+            sample_frequency=sample_rate,
+            num_mel_bins=self.fbank_config['feature_dim'],
+            frame_length=self.frame_length_ms,
+            frame_shift=self.frame_shift_ms,
+            dither=self.fbank_config['dither'],
+        )
         
         return fbank, torch.tensor(self.tokenizer.encode(txt),dtype=torch.long)
         
